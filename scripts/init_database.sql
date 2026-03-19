85f559ab2ad2787c19beb5f959da023d0b039e99
tthadarnyan-maker <tthadarnyan@gmail.com>
2026-03-19 08:14:43 -0700

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,13 @@
+--Create the 'DataWarehouse' database
+Create Database Datawarehouse
+GO
+
+Use DataWarehouse
+GO
+
+--Create Schemas
+Create Schema Bronze;
+Go
+Create Schema silver;
+Go
+Create Schema Gold
